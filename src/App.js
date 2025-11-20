17fb03c3c4cf1b0d5c5736fed84c1f6279095321
Souvik-Git5 <souvik.github2002@gmail.com>
2025-11-20 17:56:54 +0530

    App.js change something

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import page_image from './tl.webp';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src = {page_image} className="App-logo" alt="logo" />
-        <p>
-          Hi, Create an account first
-        </p>
-        <div id='App-link'>
-        <a
-        className='App-Button'
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sign-up
-        </a>
-        <a
-        className='App-Button'
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login
-        </a>
-        </div>
-      </header>
-    </div>
-  );
+export default function App(){
+return (
+<Router>
+<div className="container">
+<header className="nav">
+<h2 style={{margin:0}}>My React Site</h2>
+<nav>
+<Link to="/" style={{marginRight:12}}>Home</Link>
+<Link to="/login" style={{marginRight:12}}>Login</Link>
+<Link to="/signup">Signup</Link>
+</nav>
+</header>
+
+
+<main style={{marginTop:20}}>
+<Routes>
+<Route path="/" element={<Home/>} />
+
+</Routes>
+</main>
+
+
+<footer className="footer">© {new Date().getFullYear()} My React Site</footer>
+</div>
+</Router>
+);
 }
-
-export default App;
