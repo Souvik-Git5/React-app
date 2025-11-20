20f41f4742ce80bcb503905dffc3c88ea85fa68d
Souvik-Git5 <souvik.github2002@gmail.com>
2025-11-20 18:00:28 +0530

    App.js add home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
-
 export default function App(){
 return (
 <Router>
